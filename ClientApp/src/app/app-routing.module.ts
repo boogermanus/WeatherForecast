042260b1d908293e36844510a67cfeccd002105e
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-09-20 17:28:09 -0500

    add locationsearch to UI

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
+import { LocationSearchComponent } from './location-search/location-search.component';
 
 
 const routes: Route[] = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'locationsearch', component: LocationSearchComponent},
   { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
 ];
 @NgModule({
