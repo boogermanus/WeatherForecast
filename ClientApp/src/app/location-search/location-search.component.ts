bd8bab8d8b68b91cbd64e2d2c732e3c0c4fb94f9
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-09-20 20:19:39 -0500

    changes

--- ClientApp/src/app/location-search/location-search.component.ts
+++ ClientApp/src/app/location-search/location-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-location-search',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationSearchComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  public address: FormControl = new FormControl('', Validators.required);
+  public latitude: FormControl = new FormControl(0,
+    Validators.compose([
+      Validators.required,
+      Validators.max(90),
+      Validators.min(-90)
+    ]));
+  public longitude: FormControl = new FormControl(0,
+    Validators.compose([
+      Validators.required,
+      Validators.max(180),
+      Validators.min(-180)
+    ]));
+
+  get getAddressInvalid(): boolean {
+    return !this.address.valid && this.address.touched
+    // this is dangerous
+      && !this.longitude.touched && !this.latitude.touched;
+  }
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      address: this.address,
+      latitude: this.latitude,
+      longitude: this.longitude,
+    });
+  }
 
   ngOnInit() {
   }
